0347c71ac0298f4cf57c7746fc143d06a467cae1
leith <m.lukas@rubika-edu.com>
2022-02-09 16:56:48 +0100

    Transformation grid fix

--- TerrainShader/Assets/_Scripts/PositionTransformation.cs
+++ TerrainShader/Assets/_Scripts/PositionTransformation.cs
@@ -1,15 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PositionTransformation : Transformation
-{
+public class PositionTransformation : Transformation {
+
 	public Vector3 position;
 
-	public override Matrix4x4 Matrix
-	{
-		get
-		{
+	public override Matrix4x4 Matrix {
+		get {
 			Matrix4x4 matrix = new Matrix4x4();
 			matrix.SetRow(0, new Vector4(1f, 0f, 0f, position.x));
 			matrix.SetRow(1, new Vector4(0f, 1f, 0f, position.y));
--- TerrainShader/Assets/_Scripts/TransformationGrid.cs
+++ TerrainShader/Assets/_Scripts/TransformationGrid.cs
@@ -1,95 +1,72 @@
-using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class TransformationGrid : MonoBehaviour
-{
-    public Transform prefab;
+public class TransformationGrid : MonoBehaviour {
 
-    public int gridResolution = 10;
+	public Transform prefab;
 
-    Transform[] grid;
+	public int gridResolution = 10;
 
-    List<Transformation> transformations;
+	Transform[] grid;
 
-    Matrix4x4 transformation;
+	List<Transformation> transformations;
 
-    private void Awake()
-    {
-        grid = new Transform[gridResolution * gridResolution * gridResolution];
+	Matrix4x4 transformation;
 
-        for (int i = 0, z = 0; z < gridResolution; z++)
-        {
-            for (int y = 0; y < gridResolution; y++)
-            {
-                for (int x = 0; x < gridResolution; x++, i++)
-                {
-                    grid[i] = CreateGridPoint(x, y, z);
-                }
-            }
-        }
+	void Awake () {
+		grid = new Transform[gridResolution * gridResolution * gridResolution];
+		for (int i = 0, z = 0; z < gridResolution; z++) {
+			for (int y = 0; y < gridResolution; y++) {
+				for (int x = 0; x < gridResolution; x++, i++) {
+					grid[i] = CreateGridPoint(x, y, z);
+				}
+			}
+		}
+		transformations = new List<Transformation>();
+	}
 
-        transformations = new List<Transformation>();
-    }
+	Transform CreateGridPoint (int x, int y, int z) {
+		Transform point = Instantiate<Transform>(prefab);
+		point.localPosition = GetCoordinates(x, y, z);
+		point.GetComponent<MeshRenderer>().material.color = new Color(
+			(float)x / gridResolution,
+			(float)y / gridResolution,
+			(float)z / gridResolution
+		);
+		return point;
+	}
 
-    private void Update()
-    {
-        UpdateTransformation();
+	Vector3 GetCoordinates (int x, int y, int z) {
+		return new Vector3(
+			x - (gridResolution - 1) * 0.5f,
+			y - (gridResolution - 1) * 0.5f,
+			z - (gridResolution - 1) * 0.5f
+		);
+	}
 
-        for (int i = 0, z = 0; z < gridResolution; z++)
-        {
-            for (int y = 0; y < gridResolution; y++)
-            {
-                for (int x = 0; x < gridResolution; x++, i++)
-                {
-                    grid[i].localPosition = TransformPoint(x, y, z);
-                }
-            }
-        }
-    }
+	void Update () {
+		UpdateTransformation();
+		for (int i = 0, z = 0; z < gridResolution; z++) {
+			for (int y = 0; y < gridResolution; y++) {
+				for (int x = 0; x < gridResolution; x++, i++) {
+					grid[i].localPosition = TransformPoint(x, y, z);
+				}
+			}
+		}
+	}
 
-    Transform CreateGridPoint (int x, int y, int z)
-    {
-        Transform point = Instantiate<Transform>(prefab);
-        point.localPosition = GetCoordinates(x, y, z);
-        point.GetComponent<MeshRenderer>().material.color = new Color
-            (
-                (float)x / gridResolution,
-                (float)y / gridResolution,
-                (float)z / gridResolution
-            );
-        return point;
-    }
+	void UpdateTransformation () {
+		GetComponents<Transformation>(transformations);
+		if (transformations.Count > 0) {
+			transformation = transformations[0].Matrix;
+			for (int i = 1; i < transformations.Count; i++) {
+				transformation = transformations[i].Matrix * transformation;
+			}
+		}
+	}
 
-    Vector3 GetCoordinates(int x, int y, int z)
-    {
-        return new Vector3
-            (
-                x - (gridResolution - 1) * .5f,
-                y - (gridResolution - 1) * .5f,
-                z - (gridResolution - 1) * .5f
-            );
-    }
-
-    Vector3 TransformPoint (int x, int y, int z)
-    {
-        Vector3 coordinates = GetCoordinates(x, y, z);
-
-        return transformation.MultiplyPoint(coordinates);
-    }
-
-    void UpdateTransformation()
-    {
-        GetComponents<Transformation>(transformations);
-
-        if(transformations.Count > 0)
-        {
-            transformation = transformations[0].Matrix;
-
-            for (int i = 1; i < transformations.Count; i++)
-            {
-                transformation = transformations[i].Matrix * transformation;
-            }
-        }
-    }
+	Vector3 TransformPoint (int x, int y, int z) {
+		Vector3 coordinates = GetCoordinates(x, y, z);
+		return transformation.MultiplyPoint(coordinates);
+	}
 }
