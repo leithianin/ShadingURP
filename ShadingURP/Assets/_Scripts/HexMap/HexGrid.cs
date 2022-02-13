9db8a7dcddb77e96a72634db2227b71165aaf582
Margaux LUKAS <m.lukas@rubika-edu.com>
2022-02-13 19:15:56 +0100

    Touching cells wip (URP shader problem)

--- ShadingURP/Assets/_Scripts/HexMap/HexGrid.cs
+++ ShadingURP/Assets/_Scripts/HexMap/HexGrid.cs
@@ -40,14 +40,6 @@ public class HexGrid : MonoBehaviour
         hexMesh.Triangulate(cells);
     }
 
-    private void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            HandleInput();
-        }
-    }
-
     void CreateCell(int x, int z, int i)
     {
         Vector3 position;
@@ -67,27 +59,14 @@ public class HexGrid : MonoBehaviour
         label.text = cell.coordinates.ToStringInColumn();
     }
 
-    void HandleInput()
-    {
-        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if(Physics.Raycast(inputRay, out hit))
-        {
-            TouchCell(hit.point);
-        }
-    }
-
-    void TouchCell(Vector3 position)
+    public void ColorCell(Vector3 position, Color color)
     {
         position = transform.InverseTransformPoint(position);
         HexCoordinates coordinates = HexCoordinates.FromPosition(position);
 
         int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
         HexCell cell = cells[index];
-        cell.color = touchedColor;
+        cell.color = color;
         hexMesh.Triangulate(cells);
-
-        Debug.Log($"Touched at {coordinates.ToString()}");
     }
 }
